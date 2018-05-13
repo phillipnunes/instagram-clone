a01c5b42c8967157204fd95e636fb9c709b3e3ba
Phillip Nunes <phillip_nunes@hotmail.com>
2018-05-13 12:02:30 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { DetailsComponent } from './details/details.component';
+import { CardsComponent } from './cards/cards.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    DetailsComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
